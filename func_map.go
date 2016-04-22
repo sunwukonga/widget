3b6a00613592a2e6bf06dee4815b6390b2569847
Jinzhu <wosmvp@gmail.com>
2016-04-22 11:50:44 +0800

    Add default scope for widget

--- func_map.go
+++ func_map.go
@@ -2,6 +2,10 @@ package widget
 
 var funcMap = map[string]interface{}{
 	"widget_available_scopes": func() (results []string) {
+		if len(registeredScopes) > 0 {
+			results = append(results, "Default")
+		}
+
 		for _, scope := range registeredScopes {
 			results = append(results, scope.Name)
 		}
