4fa066fe8de46f69eab6b60a785ae571e7bfb971
Jason weng <wengjianm@gmail.com>
2016-08-31 13:23:52 +0900

    Fix widget inline edit JS bugs

--- views/themes/widget/assets/javascripts/widget_inline_edit/widget_inline_edit.js
+++ views/themes/widget/assets/javascripts/widget_inline_edit/widget_inline_edit.js
@@ -13,12 +13,14 @@
 
   'use strict';
 
+  var $body = $("body");
   var NAMESPACE = 'qor.widget.inlineEdit';
   var EVENT_ENABLE = 'enable.' + NAMESPACE;
   var EVENT_DISABLE = 'disable.' + NAMESPACE;
   var EVENT_CLICK = 'click.' + NAMESPACE;
-  var EDIT_WIDGET_BUTTON = '.qor-widget-button, .qor-slideout__close';
-  var INLINE_EDIT_URL = "";
+  var EDIT_WIDGET_BUTTON = '.qor-widget-button';
+  var ID_WIDGET = '#qor-widget-iframe';
+  var INLINE_EDIT_URL;
 
   function QorWidgetInlineEdit(element, options) {
     this.$element = $(element);
@@ -35,23 +37,21 @@
     },
 
     bind: function () {
-      this.$element.on(EVENT_CLICK, $.proxy(this.click, this));
+      this.$element.on(EVENT_CLICK, EDIT_WIDGET_BUTTON, this.click);
     },
 
     initStatus : function () {
-      $("body").append('<iframe id="qor-widget-iframe" src="' + INLINE_EDIT_URL + '"></iframe>');
+      $body.append('<iframe id="qor-widget-iframe" src="' + INLINE_EDIT_URL + '"></iframe>');
     },
 
-    click: function (e) {
-      var $target = $(e.target);
-      e.stopPropagation();
+    click: function () {
+      var $this = $(this);
 
-      if ($target.is(EDIT_WIDGET_BUTTON)){
-        $("#qor-widget-iframe").contents().find(".js-widget-edit-link").attr("data-url", $target.data("url"));
-        $("#qor-widget-iframe").addClass("show");
-        $("#qor-widget-iframe").focus();
-        $("body").addClass("open-widget-editor");
-      }
+      $(ID_WIDGET).addClass("show").focus();
+      $(ID_WIDGET).contents().find(".js-widget-edit-link").attr("data-url", $this.data("url"));
+      $body.addClass("open-widget-editor");
+
+      return false;
     }
   };
 
@@ -76,22 +76,21 @@
     });
   };
 
-  QorWidgetInlineEdit.isScrollToBottom = function (element) {
-    return element.clientHeight + element.scrollTop === element.scrollHeight;
-  };
 
   $(function () {
-    $("body").attr("data-toggle", "qor.widgets");
-    $(".qor-widget").each(function (i, e) {
-      var $wrap = $(e).find("*").eq(0);
-      INLINE_EDIT_URL = $(e).data("widget-inline-edit-url");
-      $wrap.css("position", "relative").addClass("qor-widget").attr("data-url", $(e).data("url")).unwrap();
-      $wrap.append('<div class="qor-widget-embed-wrapper"><button data-url=\"' + $(e).data("url") + '\" class="qor-widget-button">Edit</button></div>');
+    $body.attr("data-toggle", "qor.widgets");
+    $(".qor-widget").each(function () {
+      var $this = $(this);
+      var $wrap = $this.children().eq(0);
+      INLINE_EDIT_URL = $this.data("widget-inline-edit-url");
+      $wrap.css("position", "relative").addClass("qor-widget").unwrap();
+      $wrap.append('<div class="qor-widget-embed-wrapper"><button data-url=\"' + $this.data("url") + '\" class="qor-widget-button">Edit</button></div>');
     });
+    
     window.closeWidgetEditBox = function () {
-      $("#qor-widget-iframe").removeClass("show");
-      $("#qor-widget-iframe")[0].contentWindow.location.reload();
-      $("body").removeClass("open-widget-editor");
+      $(ID_WIDGET).removeClass("show");
+      $body.removeClass("open-widget-editor");
+      $(ID_WIDGET)[0].contentWindow.location.reload();
     };
 
     var selector = '[data-toggle="qor.widgets"]';
